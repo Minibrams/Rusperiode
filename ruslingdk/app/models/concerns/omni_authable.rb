644867e6c2b872a809a2281f163c76f833010752
Frederik Spang <frederik@progras.dk>
2017-10-31 21:46:48 +0100

    Use activeSupport::Concern for my concern instead of a PORO

--- ruslingdk/app/models/concerns/omni_authable.rb
+++ ruslingdk/app/models/concerns/omni_authable.rb
@@ -1,19 +1,16 @@
+require 'active_support/concern'
+
 module OmniAuthable
-  def self.included(base)
-    base.extend ClassMethods
-    base.class_eval do
-      scope :disabled, -> { where(disabled: true) }
-    end
+  extend ActiveSupport::Concern
+
+  included do
+    scope :disabled, -> { where(disabled: true) }
   end
 
-  module ClassMethods
+  class_methods do
     def find_or_create_from_authhash(authhash)
       return self.send("provider_#{authhash.provider}") if self.respond_to?("provider_#{authhash.provider}")
       nil
-    end
-
-    def provider_developer
-      # returns User
     end
   end
 end
