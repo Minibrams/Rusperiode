b189388a32d762c917d7669bc7a7e367dfdf8c0c
Frederik Spang <frederik@progras.dk>
2017-10-31 20:53:06 +0100

    Add users

---
+++ ruslingdk/app/controllers/public/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Public::SessionsController < PublicApplicationController
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
