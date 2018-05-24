5776799d3c6b1374759f0cb7431c5cdfa802d656
Bogi <bogi1511@gmail.com>
2018-05-24 13:58:41 +0200

    Fixed feedback, added actual params to pages.rb in admin and converted html encoding of æøå to actuaæ æøå

--- ruslingdk/app/admin/pages.rb
+++ ruslingdk/app/admin/pages.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Page do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :list, :of, :attributes, :on, :model
+  permit_params :educational_domain_id, :slug, :title, :view_file, :content, :accordion
   #
   # or
   #
