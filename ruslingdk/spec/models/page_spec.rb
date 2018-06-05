36cbe160feb7314e0038f482b26cb40c06087648
Frederik Spang <frederik@progras.dk>
2018-06-05 11:34:13 +0200

    Fix spec for scoped_to

--- ruslingdk/spec/models/page_spec.rb
+++ ruslingdk/spec/models/page_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Page, type: :model do
   it { should belong_to(:educational_domain) }
-  it { should validate_uniqueness_of :slug }
+  it { should validate_uniqueness_of(:slug).scoped_to(:educational_domain_id) }
 end
