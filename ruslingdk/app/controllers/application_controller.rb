bb0c74451ae39ac8524c4201fd6700387dfce073
Frederik Spang <frederik@progras.dk>
2018-06-05 15:29:33 +0200

    Easy on the cache mate

--- ruslingdk/app/controllers/application_controller.rb
+++ ruslingdk/app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 private
 
   def set_domain
-    @domain = Rails.cache.fetch("DomainCache/#{request.host}", expires_in: 12.hours) do
+    @domain = Rails.cache.fetch("DomainCache/#{request.host}", expires_in: 1.minutes) do
       EducationalDomain.find_by(domain: request.host) || EducationalDomain.default_domain
     end
   end
