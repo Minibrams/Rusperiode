3aae6341c4887d7aac586c639aad702fbb47100d
Bogi <bogi1511@gmail.com>
2018-06-05 14:34:39 +0200

    Default domain ændret til rusling.dk

--- ruslingdk/app/models/educational_domain.rb
+++ ruslingdk/app/models/educational_domain.rb
@@ -10,7 +10,7 @@ class EducationalDomain < ApplicationRecord
   belongs_to :default_page, class_name: 'Page', optional: true
   # belongs_to :layout
 
-  scope :default_domain, -> { where(domain: 'localhost').first }
+  scope :default_domain, -> { where(domain: 'rusling.dk').first }
 
 
 end
