1c993957ae2dc3fbffdac3d55f98f2cc56b672f5
Bogi <bogi1511@gmail.com>
2018-06-13 11:23:46 +0200

    refactoring of rspecs

--- ruslingdk/spec/factories/page.rb
+++ ruslingdk/spec/factories/page.rb
@@ -1,13 +1,41 @@
 FactoryBot.define do
   factory :page do
-    slug "testaccordion"
-    view_file "accordion"
-    title "testpage"
+    slug "testpage"
+    title "testtitle"
     content "testcontent"
-    association :educational_domain
-    add_attribute(:accordion) {
-      [{"title" => "testtitle", "content" => "testcontent"}, {"title" => "testtitle2", "content" => "testcontent2"}]
-    }
+
+    trait :with_domain do
+      association :educational_domain
+    end
+
+    trait :accordion do
+      slug "testaccordion"
+      view_file "accordion"
+      add_attribute(:accordion) {
+        [{"title" => "testtitle", "content" => "testcontent"}, {"title" => "testtitle2", "content" => "testcontent2"}]
+      }
+    end
+
+    trait :contacts do
+      slug "testcontacts"
+      view_file "contacts"
+    end
+
+    trait :frontpage do
+      slug "testfrontpage"
+      view_file "frontpage"
+    end
+
+    trait :index do
+      slug "testindex"
+      view_file "index"
+    end
+
+    trait :show do
+      slug "testshow"
+      view_file "show"
+    end
+
   end
 
 end
