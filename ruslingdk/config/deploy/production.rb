4530f8a1e4e0109f1228123f11acd3d5557d9596
Frederik Spang <frederik@progras.dk>
2018-07-10 16:35:28 +0200

    Fix production deployment config

--- ruslingdk/config/deploy/production.rb
+++ ruslingdk/config/deploy/production.rb
@@ -42,7 +42,7 @@ set :puma_init_active_record, true # Change to false when not using ActiveRecord
 set :nginx_config_name, "#{fetch(:application)}_#{fetch(:stage)}"
 set :nginx_flags, 'fail_timeout=0'
 set :nginx_http_flags, fetch(:nginx_flags)
-set :nginx_server_name, 'rusling.dk www.rusling.dk 192.38.56.109'
+set :nginx_server_name, 'rusling.dk *.rusling.dk 192.38.56.109'
 set :nginx_sites_available_path, '/etc/nginx/sites-available'
 set :nginx_sites_enabled_path, '/etc/nginx/sites-enabled'
 set :nginx_socket_flags, fetch(:nginx_flags)
